d9b63506dda98feac282ce6aecce91e5a286cf80
strength of faculty <strengthofaculty@gmail.com>
2025-08-11 10:27:24 +0000

    google account sign in isn't working ?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: "teacherconnect-dn75g",
+  appId: "1:707784422970:web:c8eafe935520068f4fe703",
+  storageBucket: "teacherconnect-dn75g.firebasestorage.app",
+  apiKey: "AIzaSyCYvyO91hJY-gBlaLo1aZ4FlBc7GIEGEuI",
+  authDomain: "teacherconnect-dn75g.firebaseapp.com",
+  messagingSenderId: "707784422970",
 };
 
 // Initialize Firebase
